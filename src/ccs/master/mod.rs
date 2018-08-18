69f09fc1b40b22c26aed86f770235c1ddec5181f
Maxym Naumchyk <max.naumch@gmail.com>
2018-08-19 01:43:06 +0300

    New Master

--- src/ccs/master/mod.rs
+++ src/ccs/master/mod.rs
@@ -10,3 +10,16 @@ pub use self::interface_source::*;
 
 mod implementer_source;
 pub use self::implementer_source::*;
+
+/// Master controls all allocations, lists, sets, maps, trees and other stuff
+/// of CCS.
+pub trait Master {
+
+    /// Add interface source to this master. If it is already added
+    /// then nothing is done.
+    fn add_interface_source(&mut self, isrc: InterfaceSource);
+
+    /// Add implementer source to this master. If it is already added
+    /// then nothing is done.
+    fn add_implementer_source(&mut self, isrc: ImplementerSource);
+}
