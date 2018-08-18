fe96ff381d668459c85c8619e6b692676b163e81
Maxym Naumchyk <max.naumch@gmail.com>
2018-08-18 23:19:38 +0300

    Implement Channel and new trait ChannelImplementer

--- src/ccs/handles/channel.rs
+++ src/ccs/handles/channel.rs
@@ -14,7 +14,7 @@ pub struct Channel {
     origin: Service,
 }
 
-/// Error which appers when using Channel.
+/// Error which appears when using Channel.
 pub enum ChannelError {
 
     /// This service is no longer a member
@@ -30,7 +30,32 @@ pub enum ChannelError {
     JoinForbiden,
 }
 
-type ChannelResult<T> = Result<T, ChannelError>;
+pub type ChannelResult<T> = Result<T, ChannelError>;
+
+/// Trait for channel access implementers.
+pub trait ChannelImplementer {
+
+    /// Leave this channel. Service which called this fn leaves the channel
+    /// but still has a handle to it and can retrieve information
+    /// about the channel. If channel policy allows, Service can
+    /// join the channel again.
+    fn leave(&self) -> ChannelResult<()>;
+
+    /// Invite some service to join this channel.
+    fn invite(&self, service: &Service) -> ChannelResult<()>;
+
+    /// Try joining the channel. If this service already is a member then
+    /// error occurs but service still will remain the member.
+    /// If channel policy forbids joining without invitation then
+    /// this service won't join and receives an error.
+    fn join(&self) -> ChannelResult<()>;
+
+    /// How many threads are connected to the channel.
+    fn connections_count(&self) -> usize;
+
+    /// Channel current policy.
+    fn policy(&self) -> meta::Policy;
+}
 
 impl Channel {
 
@@ -42,36 +67,5 @@ impl Channel {
     /// See [origin service](struct.Channel.html#structfield.origin).
     pub fn origin(&self) -> &Service {
         &self.origin
-    }
-
-    /// Leave this channel. Service which called this fn leaves the channel
-    /// but still has a handle to it and can retrieve information
-    /// about the channel. If channel policy allows, Service can
-    /// join the channel again.
-    pub fn leave(&self) -> ChannelResult<()> {
-        unimplemented!()
-    }
-
-    /// Invite some service to join this channel.
-    pub fn invite(&self, service: &Service) -> ChannelResult<()> {
-        unimplemented!()
-    }
-
-    /// Try joining the channel. If this service already is a member then
-    /// error occurs but service still will remain the member.
-    /// If channel policy forbids joining without invitation then
-    /// this service won't join and receives an error.
-    pub fn join(&self) -> ChannelResult<()> {
-        unimplemented!()
-    }
-
-    /// How many threads are connected to the channel.
-    pub fn connections_count(&self) -> usize {
-        unimplemented!()
-    }
-
-    /// Channel current policy.
-    pub fn policy(&self) -> meta::Policy {
-        unimplemented!()
     }
 }
