25fbd31885bf3f1e60ce55d18145063f7b9fec0a
Maxym Naumchyk <max.naumch@gmail.com>
2018-08-19 02:47:33 +0300

    New fn in MyInterfaceSource

--- src/local/mod.rs
+++ src/local/mod.rs
@@ -4,3 +4,25 @@
 //!
 //! This is useful to test different aspects of CCS and was designed for
 //! this purpose.
+//!
+//! This module can be used as tutorial for implementing your own CCS
+//! master.
+
+use super::meta::{Interface, InterfaceSet};
+
+/// The implementer of Interface source. When application begins,
+/// we add the list of all interfaces we will use so that master could
+/// find all requested sources when they get needed.
+pub struct MyInterfaceSource {
+
+    /// The set of application interfaces.
+    ints: InterfaceSet,
+}
+
+impl MyInterfaceSource {
+
+    /// Append new interface.
+    pub fn append(&mut self, i: Interface) {
+        self.ints.insert(i);
+    }
+}
