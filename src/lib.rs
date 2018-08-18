0b490ada298041a4ccd26f7617ab48da9f421c1d
Maxym Naumchyk <max.naumch@gmail.com>
2018-08-19 02:12:53 +0300

    Removed unnecessary mod ccs

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,11 @@
 /// Main module of the CCS user library. Traits presented in this module
 /// are implementing handles for objects, channels and services and not
 /// direct information.
-pub mod ccs {
 
-    pub mod meta;
+pub mod meta;
 
-    pub mod master;
+pub mod master;
 
-    pub mod local;
+pub mod local;
 
-    pub mod tools;
-}
+pub mod tools;
