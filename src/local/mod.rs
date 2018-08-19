95c4298ef9d70867e8bf9f26e9deaa5a5a03f095
Maxym Naumchyk <max.naumch@gmail.com>
2018-08-19 18:28:39 +0300

    Added links to docs

--- src/local/mod.rs
+++ src/local/mod.rs
@@ -6,24 +6,30 @@
 //! own CCS architecture.
 //!
 //! The recomended order of trait implementation by is:
-//! 1. Service entry. Service trait wants to know how to start a service
+//! 1. [Service entry](../meta/trait.ServiceEntry.html).
+//!    Service trait wants to know how to start a service
 //!    when it gets called.
-//! 2. Service architecture. Object trait needs to know the type of
+//! 2. [Service architecture](../meta/trait.ServiceArchitecture.html).
+//!    Object trait needs to know the type of
 //!    services it stores in it's collections.
-//! 3. Object architecture. Actual object with associated service type.
+//! 3. [Object architecture](../meta/trait.ObjectArchitecture.html).
+//!    Actual object with associated service type.
 //!    They have dependency on each other so they can be implemented
 //!    in reversed order.
-//! 4. Channel architecture. Depends on service implementation.
+//! 4. [Channel architecture](../meta/trait.ChannelArchitecture.html).
+//!    Depends on service implementation.
 //!    Allows to connect services and carries out data sharing.
-//! 5. Memory architecture. Used by thread to access its memory.
-//! 6. Thread architecture. Thread type that is created after some
+//! 5. [Memory architecture](../meta/trait.MemoryArchitecture.html).
+//!    Used by thread to access its memory.
+//! 6. [Thread architecture](../meta/trait.ThreadArchitecture.html).
+//!    Thread type that is created after some
 //!    service gets requested. Depends on service implementation.
-//! 7. Implementer source.
+//! 7. [Implementer source](../master/trait.ImplementerSource.html).
 //!    Source of interface implementers that is used by the master
 //!    to load load them. Depends on object implementation.
-//! 8. Interface source.
+//! 8. [Interface source](../master/trait.InterfaceSource.html).
 //!    Source of interfaces used by the master.
-//! 9. Master.
+//! 9. [Master](../master/trait.Master.html).
 //!    Main controller that controls all CCS environment.
 
 use super::meta::{Interface, InterfaceSet, Object};
